34696632a90b391b39ff49b131ad4c802146ae32
miquel <miquel@MIQUEL-PCCARLES>
2018-01-12 14:53:42 +0100

    Add project files.

---
+++ TriaCulturaDesktopApp/ViewModel/MenuViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriaCulturaDesktopApp.ViewModel
+{
+    class MenuViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
