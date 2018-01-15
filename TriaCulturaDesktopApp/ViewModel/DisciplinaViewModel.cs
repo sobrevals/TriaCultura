ea28243baf7e58b978d3a3f80f7f120f87cfb770
demian <deliriux@gmail.com>
2018-01-15 16:53:54 +0100

    Added MvvmLight, DisciplinesViewModel v0.2

--- TriaCulturaDesktopApp/ViewModel/DisciplinaViewModel.cs
+++ TriaCulturaDesktopApp/ViewModel/DisciplinaViewModel.cs
@@ -6,6 +6,8 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using TriaCulturaDesktopApp.Model;
+using MvvmDialogs.ViewModels;
+using GalaSoft.MvvmLight.Command;
  
 
 namespace TriaCulturaDesktopApp.ViewModel
@@ -13,15 +15,35 @@ namespace TriaCulturaDesktopApp.ViewModel
     class DisciplinaViewModel : INotifyPropertyChanged
     {
         triaculturaEntities context = new Model.triaculturaEntities();
+        #region BasicProperties
 
+        private List<discipline> _disciplines;
+        private discipline _selectedDiscipline;
+
+        public List<discipline> Disciplines
+        { get { return _disciplines; } set { _disciplines = value; } }
+
+        public discipline SelectedDiscipline { get { return _selectedDiscipline; } set { _selectedDiscipline = value; } }
+        #endregion
+
+        private ObservableCollection<IDialogViewModel> _dialogs = new ObservableCollection<IDialogViewModel>();
+
+        #region PropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
 
+
+       
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
+        }
+        #endregion
+        public DisciplinaViewModel ()
+        {
+            FillDisciplines(0);
         }
     }
 }
