884b138bd6a34fd6fba07e32e550764c7fd745b7
demian <deliriux@gmail.com>
2018-01-15 15:48:40 +0100

    DisciplinaViewModel 0.1

---
+++ TriaCulturaDesktopApp/ViewModel/DisciplinaViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriaCulturaDesktopApp.Model;
+
+namespace TriaCulturaDesktopApp.ViewModel
+{
+    class DisciplinaViewModel : INotifyPropertyChanged
+    {
+        triaculturaEntities context = new Model.triaculturaEntities();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
