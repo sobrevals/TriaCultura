20b775868cedc0b8d91c9184f0d4cfa093e97261
demian <deliriux@gmail.com>
2018-01-17 16:59:12 +0100

    Disciplina carrega BBDD Disciplina serveix com a model de IUserDialog Disciplina i DisciplinaViewModel afegits a App.xaml Seguir tutorial de Miquel ...

--- TriaCulturaDesktopApp/ViewModel/DisciplinaViewModel.cs
+++ TriaCulturaDesktopApp/ViewModel/DisciplinaViewModel.cs
@@ -9,12 +9,13 @@ using TriaCulturaDesktopApp.Model;
 using MvvmDialogs.ViewModels;
 using GalaSoft.MvvmLight.Command;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace TriaCulturaDesktopApp.ViewModel
 {
-    class DisciplinaViewModel : INotifyPropertyChanged
+    class DisciplinaViewModel : INotifyPropertyChanged, IUserDialogViewModel
     {
-        triaculturaEntities context = new Model.triaculturaEntities();
+        triaculturaDBEntities context = new triaculturaDBEntities();
         #region BasicProperties
 
         private List<discipline> _disciplines;
@@ -24,15 +25,22 @@ namespace TriaCulturaDesktopApp.ViewModel
         { get { return _disciplines; } set { _disciplines = value; } }
 
         public discipline SelectedDiscipline { get { return _selectedDiscipline; } set { _selectedDiscipline = value; } }
+
+        public bool IsModal
+        {
+            get
+            {
+                return true;
+            }
+        }
         #endregion
 
         private ObservableCollection<IDialogViewModel> _dialogs = new ObservableCollection<IDialogViewModel>();
 
         #region PropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler DialogClosing;
 
-
-       
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
             if (PropertyChanged != null)
@@ -40,10 +48,42 @@ namespace TriaCulturaDesktopApp.ViewModel
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
             }
         }
+
+        public void RequestClose()
+        {
+            this.DialogClosing(this, null);
+        }
         #endregion
         public DisciplinaViewModel ()
         {
             FillDisciplines(0); // crear model 
         }
+        #region fill
+        private void FillDisciplines (int index)
+        {
+            Disciplines = context.disciplines.OrderBy(x => x.type).ToList();
+            if (Disciplines != null && index >= 0 && index < Disciplines.Count)
+            {
+                SelectedDiscipline = Disciplines[index];
+            }
+        }
+        #endregion
+        #region Commands
+        public ICommand OkCommand { get { return new RelayCommand(Ok); } }
+        protected virtual void Ok()
+        {
+            if (this.OnOk != null)
+            {
+                this.OnOk(this);
+            } else
+            {
+               
+            }
+        }
+
+        public Action<DisciplinaViewModel> OnOk { get; set; }
+        public Action<DisciplinaViewModel> OnCancel { get; set; }
+        public Action<DisciplinaViewModel> OnCloseRequest { get; set; }
+        #endregion
     }
 }
