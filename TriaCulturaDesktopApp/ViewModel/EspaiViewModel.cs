341d8845070bc2ba5b5cf7753beacbf071a71b7a
Xavi <Xavi@DESKTOP-7CVBGPL>
2018-01-17 15:46:42 +0100

    .

---
+++ TriaCulturaDesktopApp/ViewModel/EspaiViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using TriaCulturaDesktopApp.Model;
+using MvvmDialogs.ViewModels;
+using GalaSoft.MvvmLight.Command;
+namespace TriaCulturaDesktopApp.ViewModel
+{
+    class EspaiViewModel: INotifyPropertyChanged
+    {
+        triaculturaEntities context = new Model.triaculturaEntities();
+
+    }
+}
