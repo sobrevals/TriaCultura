b6f1c8acb9f557d3ab482577780c0e2e85b5d192
miquel <miquel_sobrevals_@hotmail.com>
2018-01-18 15:45:09 +0100

    d

--- TriaCulturaDesktopApp/ViewModel/ProjecteViewModel.cs
+++ TriaCulturaDesktopApp/ViewModel/ProjecteViewModel.cs
@@ -4,54 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using TriaCulturaDesktopApp.Model;
 
 namespace TriaCulturaDesktopApp.ViewModel
 {
     class ProjecteViewModel
     {
-        public ProjecteViewModel()
-        {
-            _canExecute = true;
-        }
-        private ICommand _clickCommand;
-        public ICommand ClickCommand
-        {
-            get
-            {
-                return _clickCommand ?? (_clickCommand = new CommandHandler(() => MyAction(), _canExecute));
-            }
-        }
-        private bool _canExecute;
-        public void MyAction()
-        {
-            /*base.OnStartup(e);
-            WpfMvvmTest.MainWindow window = new MainWindow();
-            ProductViewModel VM = new ProductViewModel();
-            window.DataContext = VM;
-            window.Show();*/
-        }
-    }
-    public class CommandHandler : ICommand
-    {
-        private Action _action;
-        private bool _canExecute;
-        public CommandHandler(Action action, bool canExecute)
-        {
-            _action = action;
-            _canExecute = canExecute;
-        }
+        triaculturaCTXEntities context = new triaculturaCTXEntities();
 
-        public bool CanExecute(object parameter)
-        {
-            return _canExecute;
-        }
 
-        public event EventHandler CanExecuteChanged;
 
-        public void Execute(object parameter)
-        {
-            _action();
-        }
     }
 }
