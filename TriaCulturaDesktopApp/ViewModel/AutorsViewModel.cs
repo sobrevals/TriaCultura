80beb8fdec24f1fb285724bd803783e02add9277
demian <deliriux@gmail.com>
2018-01-18 16:16:34 +0100

    Arreglados App.Config (conexion bd) y rebuild del proyecto Actualizado DisciplinaViewModel y Disciplines.xaml

--- TriaCulturaDesktopApp/ViewModel/AutorsViewModel.cs
+++ TriaCulturaDesktopApp/ViewModel/AutorsViewModel.cs
@@ -4,21 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MvvmDialogs.ViewModels;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
 
 namespace TriaCulturaDesktopApp.ViewModel
 {
-   public class Autors : IUserDialogViewModel
+    public class AutorsViewModel //: IUserDialogViewModel
     {
         #region IUserDialogViewModel Implementation
 
         public bool IsModal { get; private set; }
-        public virtual void RequestClose()
-        {
-            if (this.OnCloseRequest != null)
-                this.OnCloseRequest(this);
-            else
-                Close();
-        }
+        //public virtual void RequestClose()
+        //{
+        //    if (this.OnCloseRequest != null)
+        //        this.OnCloseRequest(this);
+        //    else
+        //        Close();
+        //}
         public event EventHandler DialogClosing;
 
 
