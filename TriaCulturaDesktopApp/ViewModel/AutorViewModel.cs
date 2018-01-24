c8c98516aa1ddb4a1e79023eef0a135ca59a54d5
demian <deliriux@gmail.com>
2018-01-24 16:37:06 +0100

    Modificat Autor i els subsegüents dialegs

--- TriaCulturaDesktopApp/ViewModel/AutorViewModel.cs
+++ TriaCulturaDesktopApp/ViewModel/AutorViewModel.cs
@@ -26,12 +26,19 @@ namespace TriaCulturaDesktopApp.ViewModel
         private List<phone> _telefons;
         private List<email> _emails;
         private Image _foto;
+        private phone _selectedPhone;
+        private email _selectedEmail;
+
 
         public author Author { get { return _author; } set { _author = value; } }
         public List<discipline> Disciplines { get { return _disciplines; } set { _disciplines= value; } }
         public List<phone> Telefons { get { return _telefons; } set { _telefons= value; } }
         public List<email> Emails{ get { return _emails; } set { _emails= value; } }
         public Image Foto { get { return _foto; } set { _foto = value; } }
+
+        public phone SelectedPhone { get { return _selectedPhone; } set { _selectedPhone = value; } }
+
+        public email SelectedEmail { get { return _selectedEmail; } set { _selectedEmail = value; } }
 
         public String Titol { get; set; }
 
@@ -69,12 +76,24 @@ namespace TriaCulturaDesktopApp.ViewModel
         {
             Disciplines = Author.disciplines.ToList();
         }
+
+        public void FillTelefons ()
+        {
+            Telefons = Author.phones.ToList();
+        }
+
+        public void FillEmails ()
+        {
+            Emails = Author.emails.ToList();
+        }
+
         #endregion
         public void RequestClose()
         {
             if (this.DialogClosing != null)
                 this.DialogClosing(this, new EventArgs());
         }
+
         #region ICommand
         public ICommand OkCommand { get { return new RelayCommand(Ok); } }
         protected virtual void Ok()
@@ -85,15 +104,174 @@ namespace TriaCulturaDesktopApp.ViewModel
                 Close();
         }
 
+        public ICommand ObreDisciplines { get { return new RelayCommand(OpenDisciplines); } }
+
+        public void OpenDisciplines()
+        {
+            this.Dialogs.Add(new DisciplinaViewModel
+            {
+                SelectedAuthor = this.Author,
+                OnOk = (sender) =>
+                {
+                    context.SaveChanges();
+                    FillDisciplines();
+                },
+                OnCancel = (sender) => { sender.Close(); },
+                OnCloseRequest = (sender) => { sender.Close(); }
+            });
+        }
+
+        public ICommand AfegirTelefon { get { return new RelayCommand(AddTelefon); } }
+        public ICommand AfegirEmail { get { return new RelayCommand(AddEmail); } }
+
+        public void AddTelefon()
+        {
+            phone aux_phone = new phone();
+            this.Dialogs.Add(new AutorDialogViewModel
+            {
+                Title = "Afegir Telefon",
+                Telefon = aux_phone,
+                DataText = "Numero",
+                OkText = "Afegeix",
+                OnOk = (sender) =>
+                {
+                    context.phones.Add(aux_phone);
+                    try
+                    {
+                        context.SaveChanges();
+                        FillTelefons();
+                    } catch (Exception ex)
+                    {
+                        MessageBox.Show("Error en escriure a la BBDD.");
+                    }
+                    sender.Close();
+                },
+                OnCancel = (sender) => { sender.Close(); },
+                OnCloseRequest = (sender) => { sender.Close(); }
+            });
+        }
+        public void AddEmail()
+        {
+            email aux_mail = new email();
+            this.Dialogs.Add(new AutorDialogViewModel
+            {
+                Title = "Afegir Telefon",
+                Mail = aux_mail,
+                DataText = "Adreça",
+                OkText = "Afegeix",
+                OnOk = (sender) =>
+                {
+                    context.emails.Add(aux_mail);
+                    try
+                    {
+                        context.SaveChanges();
+                        FillTelefons();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error en escriure a la BBDD.");
+                    }
+                    sender.Close();
+                },
+                OnCancel = (sender) => { sender.Close(); },
+                OnCloseRequest = (sender) => { sender.Close(); }
+            });
+
+
+        }
+        public ICommand EsborrarTelefon { get { return new RelayCommand(DeleteTelefon); } }
+
+        public void DeleteTelefon()
+        {
+            if (SelectedPhone != null)
+            {
+                phone aux_phone = new phone();
+                aux_phone.id_phone = SelectedPhone.id_phone;
+                aux_phone.num = SelectedPhone.num;
+                aux_phone.type = SelectedPhone.type;
+                this.Dialogs.Add(new AutorDialogViewModel
+                {
+                    Title = "Esborrar Telèfon",
+                    Telefon = aux_phone,
+                    Id_item = aux_phone.id_phone,
+                    Type_item = aux_phone.type,
+                    DataText = "Numero",
+                    Data_item = aux_phone.num,
+                    TextEnabled = false,
+                    OkText = "Esborra",
+                    OnOk = (sender) =>
+                    {
+                        try
+                        {
+                            context.phones.Remove(aux_phone);
+                            context.SaveChanges();
+                            FillTelefons();
+                        } catch (Exception ex)
+                        {
+                            MessageBox.Show("No es pot esborrar aquest telèfon.");
+                        }
+                    },
+                    OnCancel = (sender) => { sender.Close(); },
+                    OnCloseRequest = (sender) => { sender.Close(); }
+
+                });
+            }
+        }
+
+        public ICommand EsborrarEmail { get { return new RelayCommand(DeleteEmail); } }
+
+        public void DeleteEmail()
+        {
+            if (SelectedEmail!= null)
+            {
+                email aux_email = new email();
+                aux_email.id_email= SelectedEmail.id_email;
+                aux_email.address= SelectedEmail.address;
+                this.Dialogs.Add(new AutorDialogViewModel
+                {
+                    Title = "Esborrar Telèfon",
+                    Mail = aux_email,
+                    Id_item = aux_email.id_email,
+                    Type_item = "Email",
+                    DataText = "Adreça",
+                    Data_item = aux_email.address,
+                    TextEnabled = false,
+                    OkText = "Esborra",
+                    OnOk = (sender) =>
+                    {
+                        try
+                        {
+                            context.emails.Remove(aux_email);
+                            context.SaveChanges();
+                            FillEmails();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("No es pot esborrar aquesta adreça.");
+                        }
+                    },
+                    OnCancel = (sender) => { sender.Close(); },
+                    OnCloseRequest = (sender) => { sender.Close(); }
+
+                });
+            }
+        }
+
         public ICommand OpenProjecte { get { return new RelayCommand(opProject); } }
         protected virtual void opProject()
         {
-            this.Dialogs.Add(new ProjecteViewModel());
+            this.Dialogs.Add(new ProjectesViewModel
+            {
+                ProjectsL = Author.projects.ToList()
+            });
         }
 
         public ICommand tornarEnrere { get { return new RelayCommand(Close); } }
         public Action<AutorViewModel> OnOk { get; set; }
+
+     
         #endregion ICommand
+
         #region DialogClosing
 
         public void Close()
