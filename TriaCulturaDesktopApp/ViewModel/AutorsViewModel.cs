b618910508f877e8ed8dd71837ca53ce251a87e6
miquel <miquel_sobrevals_@hotmail.com>
2018-01-25 15:08:42 +0100

    s

--- TriaCulturaDesktopApp/ViewModel/AutorsViewModel.cs
+++ TriaCulturaDesktopApp/ViewModel/AutorsViewModel.cs
@@ -88,7 +88,14 @@ namespace TriaCulturaDesktopApp.ViewModel
                 Author = SelectedAuthor
             });
         }
-
+        public ICommand modificarAutor { get { return new RelayCommand(changeAutor); } }
+        protected virtual void changeAutor()
+        {
+            this.Dialogs.Add(new AutorViewModel
+            {
+                Author = SelectedAuthor
+            });
+        }
         public ICommand tornarEnrere { get { return new RelayCommand(Close); } }
         public Action<AutorsViewModel> OnOk { get; set; }
         #endregion ICommand
